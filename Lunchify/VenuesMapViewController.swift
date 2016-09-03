6c01027d1ad9b23fdd2b18d5aa5354f218980ea4
Sallar Kaboli <sallar.kaboli@gmail.com>
2016-09-03 17:05:32 +0300

    Center user location in venue map view

--- Lunchify/VenuesMapViewController.swift
+++ Lunchify/VenuesMapViewController.swift
@@ -13,8 +13,39 @@ class VenuesMapViewController: VenueListViewController {
 
     @IBOutlet var mapView: MKMapView!
     
+    var pointAnnotation: MKPointAnnotation!
+    var pinAnnotationView: MKPinAnnotationView!
+    var hasBeenCentered: Bool = false
+    var userLocation: MKUserLocation?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        mapView.delegate = self
+        mapView.showsUserLocation = true
+    }
+    
     override func venuesDidLoad(venues: Venues) {
         print(venues)
     }
 
+}
+
+extension VenuesMapViewController: MKMapViewDelegate {
+    
+    func mapView(mapView: MKMapView, didUpdateUserLocation userLocation: MKUserLocation) {
+        self.userLocation = userLocation
+        
+        if (!hasBeenCentered) {
+            hasBeenCentered = true
+            let latitude = userLocation.location!.coordinate.latitude
+            let longitude = userLocation.location!.coordinate.longitude
+            let latDelta: CLLocationDegrees = 0.05
+            let lonDelta: CLLocationDegrees = 0.05
+            let span:MKCoordinateSpan = MKCoordinateSpanMake(latDelta, lonDelta)
+            let location: CLLocationCoordinate2D = CLLocationCoordinate2DMake(latitude, longitude)
+            let region: MKCoordinateRegion = MKCoordinateRegionMake(location, span)
+            mapView.setRegion(region, animated: true)
+        }
+    }
+    
 }
